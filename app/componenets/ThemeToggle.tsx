757220b7a4416317d813b8539877ff9b02c9f2ce
@nxora <omotoyedave4@gmail.com>
2025-12-04 14:29:40 +0100

    Editted themetoggle

--- app/componenets/ThemeToggle.tsx
+++ app/componenets/ThemeToggle.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"; 
+import { FaMoon, FaSun } from "react-icons/fa";
 
 export default function ThemeToggle() {
 const [theme, setTheme] = useState("dark");
@@ -14,5 +15,5 @@ localStorage.setItem("theme", next); document.documentElement.className = next; 
 
 
 return (
-<button onClick={toggle} className="px-4 py-2 rounded-lg border border-foreground/20 hover:bg-foreground/10 transition">{theme === "dark" ? "🌙" : "☀️"} </button>
+<button onClick={toggle} className="px-4 py-2 rounded-lg border border-foreground/20 hover:bg-foreground/10 transition cursor-pointer hover:scale-110">{theme === "dark" ? <FaMoon /> : <FaSun />} </button>
 );}
